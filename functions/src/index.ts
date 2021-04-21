93a220ed59c8280b7f80819454797cd8b6504ab5
thomasguillaux <thomasguillaux@gmail.com>
2021-04-21 13:18:11 +0200

    final CRUD

---
+++ functions/src/index.ts
@@ -0,0 +1,19 @@
+import * as functions from "firebase-functions";
+import * as express from "express";
+
+import {
+    addEntry,
+    deleteEntry,
+    getAllEntries,
+    updateEntry,
+} from "./entryController";
+
+const app = express();
+
+app.get("/", (req, res) => res.status(200).send("Hey there!"));
+app.post("/entries", addEntry);
+app.get("/entries", getAllEntries);
+app.patch("/entries/:entryId", updateEntry);
+app.delete("/entries/:entryId", deleteEntry);
+
+exports.app = functions.https.onRequest(app);
